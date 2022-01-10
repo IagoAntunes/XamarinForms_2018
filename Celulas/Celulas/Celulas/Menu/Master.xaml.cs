4a639fcbe08b0b61053b1e6318e2ed9669bd6da4
iagoa <iagoantunes.f@gmail.com>
2022-01-10 09:39:08 -0300

    Correçoes

--- Celulas/Celulas/Celulas/Menu/Master.xaml.cs
+++ Celulas/Celulas/Celulas/Menu/Master.xaml.cs
@@ -20,30 +20,37 @@ namespace Celulas.Menu
         private void GoPagina1(object sender,EventArgs args)
         {
             Detail = new NavigationPage(new TextCellPage());
+            IsPresented = false;
         }
         private void GoPagina2(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new ImageCellPage());
+            IsPresented = false;
         }
         private void GoPagina3(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new EntryCellPage());
+            IsPresented = false;
         }
         private void GoPagina4(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new SwitchCellPage());
+            IsPresented = false;
         }
         private void GoPagina5(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new ViewCellPage());
+            IsPresented = false;
         }
         private void GoPagina6(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new ListViewPage());
+            IsPresented = false;
         }
         private void GoPagina7(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new ListViewButtonPage());
+            IsPresented = false;
         }
     }
 }
