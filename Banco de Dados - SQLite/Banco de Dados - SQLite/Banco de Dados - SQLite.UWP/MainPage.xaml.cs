fc88aaf9fa4643a812cc39af4fe6e78b80e308af
iagoa <iagoantunes.f@gmail.com>
2022-01-10 10:11:48 -0300

    Banco de Dados - SqlLite

---
+++ Banco de Dados - SQLite/Banco de Dados - SQLite/Banco de Dados - SQLite.UWP/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Banco_de_Dados___SQLite.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new Banco_de_Dados___SQLite.App());
+        }
+    }
+}
---
+++ Banco de Dados - SQLite/Banco de Dados - SQLite/Banco de Dados - SQLite/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Banco_de_Dados___SQLite
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
